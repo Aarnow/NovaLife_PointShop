d949cc8418ff64b70f1eeaccc4fc456027e7e3db
Aarnow <arnaudfgr.dev@gmail.com>
2024-10-02 07:39:23 +0200

    fix

--- PointShop.cs
+++ PointShop.cs
@@ -101,7 +101,7 @@ namespace PointShop
             });
 
             panel.NextButton("Sélectionner", () => panel.SelectTab());
-            panel.AddButton("Retour", _ => AAMenu.AAMenu.menu.AdminPluginPanel(player, AAMenu.AAMenu.menu.AdminPluginTabLines));
+            panel.AddButton("Retour", _ => AAMenu.AAMenu.menu.AdminPluginPanel(player));
             panel.CloseButton();
 
             //Affichage
